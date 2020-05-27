df95f7898d9f433f60d758605dd9b140b9d50fb8
iThorgrim <ithogrim@outlook.com>
2020-05-27 12:11:57 +0200

    rework

--- ExpModifier_AIO_Client.lua
+++ ExpModifier_AIO_Client.lua
@@ -1,25 +1,25 @@
-local AIO = AIO or require("AIO");
+local AIO = AIO or require("AIO")
 
 if AIO.AddAddon() then
     return
 end
 
-local h_expmodifier = AIO.AddHandlers("h_expmodifier", {});
+local h_expmodifier = AIO.AddHandlers("h_expmodifier", {})
 
-local f_expmodifier = CreateFrame("Frame", "f_expmodifier", UIParent, "UIPanelDialogTemplate");
+local f_expmodifier = CreateFrame("Frame", "f_expmodifier", UIParent, "UIPanelDialogTemplate")
 
-f_expmodifier:SetSize(250, 400);
-f_expmodifier:RegisterForDrag("LeftButton");
-f_expmodifier:SetPoint("CENTER");
-f_expmodifier:SetToplevel(true);
-f_expmodifier:SetClampedToScreen(true);
+f_expmodifier:SetSize(250, 400)
+f_expmodifier:RegisterForDrag("LeftButton")
+f_expmodifier:SetPoint("CENTER")
+f_expmodifier:SetToplevel(true)
+f_expmodifier:SetClampedToScreen(true)
 -- Enable dragging of frame
-f_expmodifier:SetMovable(false);
-f_expmodifier:EnableMouse(true);
-f_expmodifier:SetScript("OnDragStart", f_expmodifier.StartMoving);
-f_expmodifier:SetScript("OnHide", f_expmodifier.StopMovingOrSizing);
-f_expmodifier:SetScript("OnDragStop", f_expmodifier.StopMovingOrSizing);
-f_expmodifier:Hide();
+f_expmodifier:SetMovable(false)
+f_expmodifier:EnableMouse(true)
+f_expmodifier:SetScript("OnDragStart", f_expmodifier.StartMoving)
+f_expmodifier:SetScript("OnHide", f_expmodifier.StopMovingOrSizing)
+f_expmodifier:SetScript("OnDragStop", f_expmodifier.StopMovingOrSizing)
+f_expmodifier:Hide()
 
 local f_expmodifier_TitleBar = f_expmodifier:CreateFontString("f_expmodifier_TitleBar")
 f_expmodifier_TitleBar:SetFont("Fonts\\FRIZQT__.TTF", 13)
@@ -31,7 +31,9 @@ local f_expmodifier_InnerText = f_expmodifier:CreateFontString("f_expmodifier_In
 f_expmodifier_InnerText:SetFont("Fonts\\FRIZQT__.TTF", 12.3)
 f_expmodifier_InnerText:SetSize(190, 0)
 f_expmodifier_InnerText:SetPoint("CENTER", 0, 105)
-f_expmodifier_InnerText:SetText("Allows you to change your experience multiplier.\n\nThis will allow you to gain more experience or less depending on your choice.")
+f_expmodifier_InnerText:SetText(
+    "Allows you to change your experience multiplier.\n\nThis will allow you to gain more experience or less depending on your choice."
+)
 
 local f_expmodifier_InnerText2 = f_expmodifier:CreateFontString("f_expmodifier_InnerText2")
 f_expmodifier_InnerText2:SetFont("Fonts\\FRIZQT__.TTF", 12.3)
@@ -39,7 +41,13 @@ f_expmodifier_InnerText2:SetSize(190, 0)
 f_expmodifier_InnerText2:SetPoint("CENTER", 0, 20)
 f_expmodifier_InnerText2:SetText("|CFFa8a8ffYour current multiplier : ")
 
-local f_expmodifier_InnerMyRate = f_expmodifier:CreateFontString("f_expmodifier_InnerMyRate", function() AIO.Handle("h_expmodifier", "getRateModifier", 1) end)
+local f_expmodifier_InnerMyRate =
+    f_expmodifier:CreateFontString(
+    "f_expmodifier_InnerMyRate",
+    function()
+        AIO.Handle("h_expmodifier", "getRateModifier", 1)
+    end
+)
 f_expmodifier_InnerMyRate:SetFont("Fonts\\FRIZQT__.TTF", 20)
 f_expmodifier_InnerMyRate:SetSize(190, 0)
 f_expmodifier_InnerMyRate:SetPoint("CENTER", 0, -10)
@@ -51,7 +59,12 @@ f_expmodifier_Button1:EnableMouse(true)
 f_expmodifier_Button1:SetNormalTexture("Interface/BUTTONS/UI-DialogBox-Button-Up")
 f_expmodifier_Button1:SetHighlightTexture("Interface/BUTTONS/UI-DialogBox-Button-Highlight")
 f_expmodifier_Button1:SetPushedTexture("Interface/BUTTONS/UI-DialogBox-Button-Down")
-f_expmodifier_Button1:SetScript("OnMouseUp", function() AIO.Handle("h_expmodifier", "setRateModifier", 1) end)
+f_expmodifier_Button1:SetScript(
+    "OnMouseUp",
+    function()
+        AIO.Handle("h_expmodifier", "setRateModifier", 1)
+    end
+)
 
 local f_expmodifier_Text1 = f_expmodifier_Button1:CreateFontString("f_expmodifier_Text1")
 f_expmodifier_Text1:SetFont("Fonts\\FRIZQT__.TTF", 12)
@@ -66,7 +79,12 @@ f_expmodifier_Button2:EnableMouse(true)
 f_expmodifier_Button2:SetNormalTexture("Interface/BUTTONS/UI-DialogBox-Button-Up")
 f_expmodifier_Button2:SetHighlightTexture("Interface/BUTTONS/UI-DialogBox-Button-Highlight")
 f_expmodifier_Button2:SetPushedTexture("Interface/BUTTONS/UI-DialogBox-Button-Down")
-f_expmodifier_Button2:SetScript("OnMouseUp", function() AIO.Handle("h_expmodifier", "setRateModifier", 2) end)
+f_expmodifier_Button2:SetScript(
+    "OnMouseUp",
+    function()
+        AIO.Handle("h_expmodifier", "setRateModifier", 2)
+    end
+)
 
 local f_expmodifier_Text2 = f_expmodifier_Button2:CreateFontString("f_expmodifier_Text2")
 f_expmodifier_Text2:SetFont("Fonts\\FRIZQT__.TTF", 12)
@@ -81,7 +99,12 @@ f_expmodifier_Button3:EnableMouse(true)
 f_expmodifier_Button3:SetNormalTexture("Interface/BUTTONS/UI-DialogBox-Button-Up")
 f_expmodifier_Button3:SetHighlightTexture("Interface/BUTTONS/UI-DialogBox-Button-Highlight")
 f_expmodifier_Button3:SetPushedTexture("Interface/BUTTONS/UI-DialogBox-Button-Down")
-f_expmodifier_Button3:SetScript("OnMouseUp", function() AIO.Handle("h_expmodifier", "setRateModifier", 3) end)
+f_expmodifier_Button3:SetScript(
+    "OnMouseUp",
+    function()
+        AIO.Handle("h_expmodifier", "setRateModifier", 3)
+    end
+)
 
 local f_expmodifier_Text3 = f_expmodifier_Button3:CreateFontString("f_expmodifier_Text3")
 f_expmodifier_Text3:SetFont("Fonts\\FRIZQT__.TTF", 12)
@@ -96,7 +119,12 @@ f_expmodifier_Button4:EnableMouse(true)
 f_expmodifier_Button4:SetNormalTexture("Interface/BUTTONS/UI-DialogBox-Button-Up")
 f_expmodifier_Button4:SetHighlightTexture("Interface/BUTTONS/UI-DialogBox-Button-Highlight")
 f_expmodifier_Button4:SetPushedTexture("Interface/BUTTONS/UI-DialogBox-Button-Down")
-f_expmodifier_Button4:SetScript("OnMouseUp", function() AIO.Handle("Kaev", "AttributesDecrease", 1) end)
+f_expmodifier_Button4:SetScript(
+    "OnMouseUp",
+    function()
+        AIO.Handle("Kaev", "AttributesDecrease", 1)
+    end
+)
 
 local f_expmodifier_Text4 = f_expmodifier_Button4:CreateFontString("f_expmodifier_Text4")
 f_expmodifier_Text4:SetFont("Fonts\\FRIZQT__.TTF", 12)
@@ -104,12 +132,12 @@ f_expmodifier_Text4:SetSize(190, 10)
 f_expmodifier_Text4:SetPoint("CENTER", 0, 5)
 f_expmodifier_Text4:SetText("Exit")
 
-AIO.SavePosition(f_expmodifier);
+AIO.SavePosition(f_expmodifier)
 
 function h_expmodifier.ShowFrame(player)
     f_expmodifier:Show()
 end
 
 function h_expmodifier.setMyRate(player, test1)
-    f_expmodifier_InnerMyRate:SetText("|CFFa8a8ff "..test1.."")
+    f_expmodifier_InnerMyRate:SetText("|CFFa8a8ff " .. test1 .. "")
 end
